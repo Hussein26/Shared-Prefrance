bfc5953362c7d5822ae66bea76c24ca670cc2bd3
Hussein26 <hussein667.tarek@gmail.com>
2022-09-24 22:22:06 +0200

    init2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,7 +20,7 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              height: 15,
+              height: 16,
             ),
             Text(
               'welcome ${_email}',
